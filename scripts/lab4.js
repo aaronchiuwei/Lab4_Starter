aa90e6170246e63e50c3e2b84637a6f5d38e64bb
Aaron Chiuwei <107961648+aaronchiuwei@users.noreply.github.com>
2025-04-29 23:37:06 -0700

    fix buggy code in lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -14,7 +14,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -25,13 +25,16 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (prices.length == 0) {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     for(let i = 0; i < length; i++) {
         let discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
-
+    
     return discounted;
 }
 
